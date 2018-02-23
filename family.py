e6771c0ac33af3b99ca80bf39ed8452a52f839a6
kingking <to_kingking@foxmail.com>
2018-02-23 09:41:25 +0800

    Upload example.py family.py

--- family.py
+++ family.py
@@ -5,13 +5,16 @@ import pyaudio
 import wave
 import numpy as np
 from pydub import AudioSegment
-import pygame
 import time
 import webbrowser
 class Family():
-    def __init__(self,name,speed):
+    def __init__(self,name=['小明'],per=1,speed=5,pit=5,vol=5,listen_time=5):
         self.name = name
         self.speed = speed
+        self.per = per
+        self.pit = pit
+        self.vol = vol
+        self.listen_time = listen_time
         self._output_file = 'audio/_output_file.wav'
         self.man_wav = 'audio/man.wav'
         self.machine_mp3 = 'audio/machine.mp3'
@@ -26,6 +29,7 @@ class Family():
         self.stop = not True
         self.starttime = 0
         self.nowtime = 0
+        self.test = True
         
     """以二进制格式打开文件"""
     def get_file_content(self,filePath):
@@ -43,11 +47,20 @@ class Family():
     def play_wav(self,file):
         """参数：wav文件绝对路径"""
         """阻塞，直到播放完成"""
-        pygame.mixer.init()  #pygame混音器初始化(mixer播放功能很好用)
-        mp3 = pygame.mixer.Sound(file)  #加载声音文件
-        mp3.play(loops=0)  #播放声音文件，参数：循环次数(其他参数查阅pygame官网)
-        while pygame.mixer.get_busy():  #等待音频播放完成
-            pass
+        CHUNK = 1024
+        wf = wave.open(file, 'rb')
+        p = pyaudio.PyAudio()
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True)
+        data = wf.readframes(CHUNK)
+        while data:
+            stream.write(data)
+            data = wf.readframes(CHUNK)
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
         
     """图灵机器人"""
     def answer(self):
@@ -112,7 +125,10 @@ class Family():
             self.recodeing()
             result_text = self.client.asr(self.get_file_content(self.man_wav), 'wav',8000, { 'lan': 'zh',})
             if 'result' in result_text.keys():
-                if result_text["result"][0][:-1] == self.name:
+                info = result_text["result"][0][:-1]
+                if self.test:
+                    print('唤醒音识别结果：'+info)
+                if info in self.name:
                     self.listen = not True
                     self.work = True
                     self.stop = not True
@@ -139,15 +155,15 @@ class Family():
                     wait = not True
                     break
                 self.nowtime = time.time()
-                if self.nowtime-self.starttime>10:
+                if self.nowtime-self.starttime>self.listen_time:
                     self.listen = True
                     self.work = not True
                     self.stop = not True
-                    self.play_wav(self.sleep_wav)
+                    self.play_wav(self.stop_wav)
                     break
             if self.work:
                 large_count = np.sum( audio_data > LEVEL )
-                while large_count>10:
+                while large_count>8:
                     frames.append(data) 
                     data = stream.read(CHUNK)
                     audio_data = np.fromstring(data, dtype=np.short)
@@ -166,15 +182,41 @@ class Family():
                 result_text = self.client.asr(self.get_file_content(self.man_wav), 'wav',8000, { 'lan': 'zh',})
                 if 'result' in result_text.keys():
                     self.questen = result_text["result"][0][:-1]
-                    turing_answer = self.answer()
-                    turing_answer_text = turing_answer['text']
-                    if 'url' in turing_answer.keys():
-                        webbrowser.open(turing_answer['url'])
-                    if 'list' in turing_answer.keys():
-                        webbrowser.open(turing_answer['list'][0]['detailurl'])
-                        webbrowser.open(turing_answer['list'][1]['detailurl'])
-                    result  = self.client.synthesis(turing_answer_text, 'zh', 1,\
-                                 {'vol': 4,'spd':self.speed,'per':4,'pit':3})
+                    if self.test:
+                        print('语音输入识别结果：'+self.questen)
+                    if self.questen in ['机电了','点了','机电','电缆','电了','现在几点了']:#对时间的处理，‘几点了’的识别结果很无奈
+                        now_time = time.localtime()
+                        tm_hour = now_time.tm_hour
+                        if tm_hour >= 12:
+                            tm_hour -= 12
+                        tm_min = now_time.tm_min
+                        tm_sec = now_time.tm_sec
+                        turing_answer_text = str(tm_hour)+'点'+str(tm_min)+'分'+str(tm_sec)+'秒'
+                    elif self.questen in ['今天几号','今天多少号']:#对日期的处理
+                        now_time = time.localtime()
+                        tm_mon = now_time.tm_mon
+                        tm_mday = now_time.tm_mday
+                        turing_answer_text = str(tm_mon)+'月'+str(tm_mday)+'号'
+                    elif self.questen in ['今天星期几']:#对星期的处理
+                        now_time = time.localtime()
+                        tm_wday = now_time.tm_wday+1
+                        if tm_wday <= 6:
+                            turing_answer_text = '星期'+str(tm_wday)
+                        else:
+                            turing_answer_text = '星期日'
+                    elif self.questen in ['唱首歌']:#对唱歌的处理
+                        turing_answer_text = '走开，我是不会给你唱歌的'
+                    else:
+                        turing_answer = self.answer()
+                        turing_answer_text = turing_answer['text']
+                        if 'url' in turing_answer.keys():
+                            webbrowser.open(turing_answer['url'])
+                        if 'list' in turing_answer.keys():
+                            webbrowser.open(turing_answer['list'][0]['detailurl'])
+                            webbrowser.open(turing_answer['list'][1]['detailurl'])
+                    if self.test:
+                        print('图灵机器人的回答：'+turing_answer_text)
+                    result  = self.client.synthesis(turing_answer_text, 'zh', 1,{'vol':self.vol,'spd':self.speed,'per':self.per,'pit':self.pit})
                     if not isinstance(result, dict):  
                         with open(self.machine_mp3, 'wb') as f:
                             f.write(result)
